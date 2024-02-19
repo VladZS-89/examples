03cbfb0f1db434b024304806424408fd06ebcd02
Vlad <zhuravlevv89@mail.ru>
2024-02-19 21:49:46 +0300

    TransactionalDemo

---
+++ src/main/java/com/study/examples/repository/SomeRepository.java
@@ -0,0 +1,7 @@
+package com.study.examples.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SomeRepository {
+}
---
+++ src/main/java/com/study/examples/service/SomeService.java
@@ -0,0 +1,7 @@
+package com.study.examples.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SomeService {
+}
